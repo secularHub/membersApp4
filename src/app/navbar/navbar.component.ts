4e4152efc24c4927239cc55124a701721d7ef1c9
dfangrow <dfangrow@gmail.com>
2017-04-26 19:58:16 -0600

    disabled navbar hover

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   ngOnInit() {
-    this.setupClickForHover();
+//    this.setupClickForHover();
   }
 
   private setupClickForHover() {
